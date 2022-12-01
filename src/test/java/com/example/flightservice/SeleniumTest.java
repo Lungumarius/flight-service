5d0611546c04c5289816a171d70b32c5599e4fae
Georgia Asimaki <Georgia.Asimaki@solera.com>
2022-12-01 13:35:40 +0200

    airline filter testing

--- src/test/java/com/example/flightservice/SeleniumTest.java
+++ src/test/java/com/example/flightservice/SeleniumTest.java
@@ -36,23 +36,50 @@ public class SeleniumTest {
             //adding a team
             WebElement input = driver.findElement(By.xpath("//div[@id='origin']"));
             input.click();
+        Thread.sleep(3000);
         WebElement input2 = driver.findElement(By.xpath("//li[@id='ath']"));
         input2.click();
+        Thread.sleep(3000);
         WebElement input3 = driver.findElement(By.xpath("//div[@id='destination']"));
         input3.click();
+        Thread.sleep(3000);
         WebElement input4 = driver.findElement(By.xpath("//li[@id='mad2']"));
         input4.click();
-
-        WebElement input6 = driver.findElement(By.xpath("//input[@id='first']"));
-        //delete the default
-        input6.clear();
-        input6.sendKeys(Keys.CONTROL + "a");
-        input6.sendKeys(Keys.DELETE);
-        //input the new one
-        input6.sendKeys("05/08/2022");
-        Thread.sleep(7000);
+        Thread.sleep(3000);
         WebElement input5 = driver.findElement(By.xpath("//a[@id='search']"));
         input5.click();
+        Thread.sleep(3000);
+
+        driver.get("http://localhost:3000/results/");
+
+        Thread.sleep(10000);
+        //adding a team
+        WebElement input6 = driver.findElement(By.xpath("//div[@id='airlineFilter']"));
+        input6.click();
+
+        Thread.sleep(3000);
+        WebElement input7 = driver.findElement(By.xpath("//li[@id='aegean']"));
+        input7.click();
+
+
+        Thread.sleep(200);
+        input6.click();
+        Thread.sleep(3000);
+        WebElement input8 = driver.findElement(By.xpath("//li[@id='iberia']"));
+        input8.click();
+
+        Thread.sleep(200);
+        input6.click();
+        Thread.sleep(3000);
+        WebElement input9 = driver.findElement(By.xpath("//li[@id='ryanair']"));
+        input9.click();
+
+        Thread.sleep(200);
+        input6.click();
+        Thread.sleep(3000);
+        WebElement input10 = driver.findElement(By.xpath("//li[@id='allAir']"));
+        input10.click();
+
 
 //            System.out.println(rowCount2);
 //            assertEquals(rowCount, rowCount2);
